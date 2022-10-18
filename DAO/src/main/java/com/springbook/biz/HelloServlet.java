c0bf0c6b93c69708527a13cbd51c17a627b5f952
hellheit2 <hellheit2@gmail.com>
2022-10-18 14:36:09 +0900

    Eclipse 스피링 ~p.130 DAO

---
+++ DAO/src/main/java/com/springbook/biz/HelloServlet.java
@@ -0,0 +1,20 @@
+package com.springbook.biz;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet {
+
+	
+	public HelloServlet() {
+		System.out.println("===> HelloServlet 객체 생성");
+	}
+	
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("doGet() 메소드 호출");
+	}
+
+}
